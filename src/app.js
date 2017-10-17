43c656636fc89ca76dfd78c3848179bad1c8a81d
Jose L Riano <joselriano@yahoo.com>
2017-10-17 10:11:01 -0400

    LifecycleMethods

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-//stateless functional component
+
 
 class IndecisionApp extends React.Component {
     constructor(props){
@@ -6,14 +6,43 @@ class IndecisionApp extends React.Component {
             this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
             this.handlePick = this.handlePick.bind(this);
             this.handleAddOption = this.handleAddOption.bind(this);
+            this.handleDeleteOption =this.handleDeleteOption.bind(this);
             this.state ={
-                options: props.options
+                options: []
             };
+    }
+    componentDidMount(){
+        try{
+            const json = localStorage.getItem('options');
+            const options = JSON.parse(json);
+            if(options){
+                this.setState(() => ({ options }));
+            }
+        }catch (e){
+            //do nothing if you get invalid data
+        }
+
+       
+        
+    }
+    componentDidUpdate(prevProps, prevState){
+        if(prevState.options.length !== this.state.options.length){
+            const json = JSON.stringify(this.state.options);
+            localStorage.setItem('options', json);
+        }    
+    }
+    componentWillUnmount(){
+        console.log('consoleWillUnmount')
     }
     handleDeleteOptions(){
         this.setState(() => ({   options: [] }));
     }
-   
+    handleDeleteOption(optionToRemove){
+        this.setState ((prevState)  => ({
+            options: prevState.options.filter((option) => optionToRemove !== option)
+        }));
+    }
+
     handlePick(){
         const randomNum = Math.floor(Math.random() * this.state.options.length);
         return alert(this.state.options[randomNum]);
@@ -25,7 +54,9 @@ class IndecisionApp extends React.Component {
             return 'This option already exists';
         }
         
-        this.setState((prevState) => ({options: prevState.options.concat(option)}));
+        this.setState((prevState) => ({
+            options: prevState.options.concat(option)
+        }));
         
     }
     render(){
@@ -41,6 +72,7 @@ class IndecisionApp extends React.Component {
                 <Options 
                     options={this.state.options} 
                     handleDeleteOptions={this.handleDeleteOptions}
+                    handleDeleteOption = {this.handleDeleteOption}
                 />
                 <AddOption 
                     handleAddOption={this.handleAddOption}
@@ -50,9 +82,6 @@ class IndecisionApp extends React.Component {
     }
 }
 
-IndecisionApp.defaultProps ={
-    options : []
-};
 const Header = (props) => {
     return (
         <div>
@@ -82,9 +111,15 @@ const Options = (props) => {
     return(
         <div>
         <button onClick={props.handleDeleteOptions}>Remove All</button>
-        
+        {props.options.length === 0 && <p>Please add an option to get started!</p>}
         {
-            props.options.map((option) => <Option key={option} optionText={option}/>)
+            props.options.map((option) => (
+                <Option 
+                    key={option} 
+                    optionText={option}
+                    handleDeleteOption = {props.handleDeleteOption}
+                />
+            ))
         }
         </div>
     );
@@ -95,6 +130,12 @@ const Option = (props) => {
     return(
         <div>
            {props.optionText}
+           <button onClick={(e) => {
+               props.handleDeleteOption(props.optionText);
+           }}
+           >
+                remove
+            </button>
         </div>
     );
 };
@@ -112,8 +153,12 @@ class AddOption extends React.Component {
 
         const option = e.target.elements.option.value.trim();
         const error = this.props.handleAddOption(option);
-        this.setState(() => ({error}));
-      
+
+        this.setState(() =>  ({ error }));
+
+        if(!error){
+            e.target.elements.option.value = '';
+        }
 
     }
     render(){
