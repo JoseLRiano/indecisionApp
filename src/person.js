d76cf170d4c73546089966a1bb73a4983533a62f
Jose L Riano <joselriano@yahoo.com>
2017-11-04 10:44:28 -0400

    Second commit

---
+++ src/person.js
@@ -0,0 +1,6 @@
+
+export const isAdult = (x) => (x > 17 ) ? 'Adult' : 'Not an Adult';
+
+export const canDrink = (y) => ( y > 20 ) ? 'Can Drink' : 'Can not Drink';
+
+export default (age) => age>= 64;
